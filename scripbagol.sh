fb03a552483efc98d007f0743baf13e2f5ef2a09
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-02 22:54:57 +0700

    Update scripbagol.sh

--- scripbagol.sh
+++ scripbagol.sh
@@ -1,11 +1,11 @@
-!apt install xrdp
-!service xrdp start
-!apt install firefox
-!apt install pciutils
-! wget https://raw.githubusercontent.com/admin123356/bagol1/main/CRD_COLAB_EXE.sh &> /dev/null
-! chmod +x CRD_COLAB_EXE.sh
-! ./CRD_COLAB_EXE.sh
-!pip install git+https://github.com/SwayamJoshi87/remocolab.xrdp.git
+pip install git+https://github.com/SwayamJoshi87/remocolab.xrdp.git
 import remocolab
 remocolab.setupVNC()
-! sleep 43200
+apt install xrdp
+service xrdp start
+apt install firefox
+apt install pciutils
+wget https://raw.githubusercontent.com/admin123356/bagol1/main/CRD_COLAB_EXE.sh &> /dev/null
+chmod +x CRD_COLAB_EXE.sh
+./CRD_COLAB_EXE.sh
+sleep 43200
