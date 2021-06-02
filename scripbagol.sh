8f141634fee73e248291a867f59f18bf153b3cfd
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-02 22:49:01 +0700

    Update and rename scrip bagol.sh to scripbagol.sh

--- scripbagol.sh
+++ scripbagol.sh
@@ -5,9 +5,7 @@
 ! wget https://raw.githubusercontent.com/admin123356/bagol1/main/CRD_COLAB_EXE.sh &> /dev/null
 ! chmod +x CRD_COLAB_EXE.sh
 ! ./CRD_COLAB_EXE.sh
-
 !pip install git+https://github.com/SwayamJoshi87/remocolab.xrdp.git
 import remocolab
 remocolab.setupVNC()
-
 ! sleep 43200
