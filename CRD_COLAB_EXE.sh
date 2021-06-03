e1f60fa8e34760a82a6eba3da61c1bc490949fa4
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-04 05:25:24 +0700

    Update CRD_COLAB_EXE.sh

--- CRD_COLAB_EXE.sh
+++ CRD_COLAB_EXE.sh
@@ -64,31 +64,6 @@ else
     exit
 fi
 
-# Installing Chrome Remote Desktop
-printf "\n$g$b    Installing Chrome Remote Desktop $endc$enda" >&2
-{
-    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-    sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    Chrome Remote Desktop Installed $endc$enda\n" >&2 ||
-{ printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
-
-
-
-# Install Desktop Environment (XFCE4)
-printf "$g$b    Installing Desktop Environment $endc$enda" >&2
-{
-    sudo DEBIAN_FRONTEND=noninteractive \
-        apt install --assume-yes xfce4 desktop-base
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-    sudo apt install --assume-yes xscreensaver
-    sudo systemctl disable lightdm.service
-} &> /dev/null &&
-printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
-{ printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
-
-
 
 # Install Google Chrome
 printf "$g$b    Installing Google Chrome $endc$enda" >&2
@@ -106,3 +81,11 @@ printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
 
 printf "\n$g$b    Installation Completed $endc$enda\n\n" >&2
+
+
+
+
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
+unzip ngrok-stable-linux-amd64.zip
+./ngrok
