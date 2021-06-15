d4919f8cd1daf8579635d1a8181444fdbe195971
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-16 04:03:26 +0700

    Update CRD_COLAB_EXE.sh

--- CRD_COLAB_EXE.sh
+++ CRD_COLAB_EXE.sh
@@ -99,47 +99,6 @@ printf "$g$b    Installing Google Chrome $endc$enda" >&2
 printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
-
-
-# Install CrossOver (Run exe on linux)
-printf "$g$b    Installing CrossOver $endc$enda" >&2
-{
-    wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
-    sudo dpkg -i crossover_20.0.2-1.deb
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    CrossOver Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-
-
-# Install OBS-Studio (Livestream)
-printf "$g$b    Installing Obs-studio $endc$enda" >&2
-{
-    sudo apt install ffmpeg -y
-    sudo add-apt-repository ppa:obsproject/obs-studio -y
-    sudo apt install obs-studio -y
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    OBS-Studio Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install VLC Media Player 
-printf "$g$b    Installing VLC Media Player $endc$enda" >&2
-{
-    sudo apt install vlc -y
-} &> /dev/null &&
-printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install xfce4-goodies 
-printf "$g$b    Installing xfce4-goodies $endc$enda" >&2
-{
-    sudo apt install xfce4-goodies -y
-} &> /dev/null &&
-printf "\r$c$b    xfce4-goodies Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
 # Install xfce4
 printf "$g$b    Installing xfce4-goodies $endc$enda" >&2
 {
@@ -169,7 +128,7 @@ fi
 
 
 # JANGAN LUPA subscriber LELED CHANNEL 
-printf "$g$b    Installing xfce4-goodies $endc$enda" >&2
+printf "$g$b JANGAN LUPA subscriber LELED CHANNEL $endc$enda" >&2
 {
 ! service xrdp start -y
 ! ./ngrok
