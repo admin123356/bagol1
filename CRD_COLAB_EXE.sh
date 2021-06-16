9cbed7e9b2085f9efc0b309bfb5887fc834c9812
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-17 00:20:48 +0700

    Update CRD_COLAB_EXE.sh

--- CRD_COLAB_EXE.sh
+++ CRD_COLAB_EXE.sh
@@ -35,7 +35,10 @@ printf """$c$b
     $r ALFIAN ADI SUKMA $c 
 $endc$enda""";
 
-
+ Finishing Work
+printf '\nVisit https://dashboard.ngrok.com/auth and Copy the command after authentication\n'
+read -p "Paste Command: " CRP
+su - user -c """$CRP"""
 
 # Used Two if else type statements, one is simple second is complex. So, don't get confused or fear by seeing complex if else statement '^^.
 
