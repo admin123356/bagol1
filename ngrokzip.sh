f5c77691e5f23ce2e14776d8bf176040212cde5d
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-21 09:42:07 +0700

    Update ngrokzip.sh

--- ngrokzip.sh
+++ ngrokzip.sh
@@ -1,2 +1,9 @@
+
+# PAKAI LAH DENGAN BIJAK
+printf "\n$g$b    PAKAI LAH DENGAN BIJAK $endc$enda" >&2
+{
 ! wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
 ! unzip ngrok-stable-linux-amd64.zip
+} &> /dev/null &&
+printf "\r$c$b    PAKAI LAH DENGAN BIJAK $endc$enda\n" >&2 ||
+{ printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
